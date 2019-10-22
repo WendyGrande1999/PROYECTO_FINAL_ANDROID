79cd59bc6da44eb68f0514fabdddb8ab93d36abd
WendyGrande1999 <elizabethalvarezgra17@gmail.com>
2019-10-22 14:32:52 -0600

    he agregado una nueva activity

---
+++ app/src/main/java/com/example/proyecto_final_android/Main2Activity2.java
@@ -0,0 +1,14 @@
+package com.example.proyecto_final_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Main2Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main22);
+    }
+}
